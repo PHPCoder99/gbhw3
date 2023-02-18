cf2cad3ba550cf9fcac9ba003988955274072caa
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2023-02-18 22:01:30 +0700

    solved 3 tasks

--- Program.cs
+++ Program.cs
@@ -4,9 +4,54 @@ namespace gbhw3
 {
     class Program
     {
+        // 1...
+        // public static string Reverse(string s){
+        //     char[] charArray = s.ToCharArray();
+        //     Array.Reverse(charArray);
+        //     return new string(charArray);
+        // }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1...
+            // Console.WriteLine("Enter a number:");
+
+            // string s = Console.ReadLine();
+
+            // if (s.Length == 5){
+            //     if (Reverse(s) == s){
+            //     Console.WriteLine("yes");
+            //     } else {
+            //         Console.WriteLine("no");
+            //     }
+            // } else {
+            //     Console.WriteLine("Invalid number");
+            // }
+
+            // 2...
+            // System.Console.WriteLine("Enter 3 numbers:");
+
+            // int[] coordsA = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            // System.Console.WriteLine("Enter 3 more numbers:");
+
+            // int[] coordsB = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            // double distanceX = Math.Pow(coordsB[0]-coordsA[0], 2);
+            // double distanceY = Math.Pow(coordsB[1]-coordsA[1], 2);
+            // double distanceZ = Math.Pow(coordsB[2]-coordsA[2], 2);
+
+            // System.Console.WriteLine(Math.Sqrt(distanceX+distanceY+distanceZ));
+
+            // 3...
+            // Console.WriteLine("Enter a number:");
+
+            // int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+            // for (int i = 1; i <= num; i++)
+            // {
+            //     Console.Write(Math.Pow(i, 3) + " ");
+            // }
         }
     }
 }
